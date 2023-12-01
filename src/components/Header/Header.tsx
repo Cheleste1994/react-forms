274b61d508f35d2a9d4e014e60ccf7e2c11c0947
Cheleste1994 <wolverines@tut.by>
2023-12-01 13:02:25 +0300

    feat: add card user in home page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../redux/hooks/hooks';
 import styles from './Header.module.scss';
 
 export default function Header(): JSX.Element {
+  const { username } = useAppSelector((store) => store.userReducer);
+
   return (
     <header className={styles.header}>
       <div>React. Forms</div>
       <div>
+        {username && <h3>Hello {username}!</h3>}
         <nav>
           <Link to="/">Home</Link>
           <Link to="/registration/uncontrolled">Uncontrolled form</Link>
--- src/routes/HomePage/HomePage.tsx
+++ src/routes/HomePage/HomePage.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import { useAppSelector } from '../../redux/hooks/hooks';
+import styles from './HomePage.module.scss';
 
 export default function HomePage(): JSX.Element {
+  const { username, photo, age, country, email, sex } = useAppSelector(
+    (store) => store.userReducer
+  );
   return (
     <>
       <Header />
-      <main data-testid="page-home">Home Page</main>
+      <main data-testid="page-home">
+        {!username && 'Home Page'}
+        <div className={styles.user}>
+          {username && <div>Name: {username}</div>}
+          {photo && (
+            <img src={photo} alt="Base64 Image" className={styles.photo} />
+          )}
+          {age && <div>Age: {age}</div>}
+          {country && <div>Country: {country}</div>}
+          {email && <div>Email: {email}</div>}
+          {sex && <div>Gender: {sex}</div>}
+        </div>
+      </main>
     </>
   );
 }
