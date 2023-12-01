3242e429986b8d63e04df2296843f01221eaa0a0
Cheleste1994 <wolverines@tut.by>
2023-12-01 13:27:16 +0300

    fix: deleted unnecessary files

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,12 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
-import productsApi from './api/productsApi';
 import countriesReducer from './slice/countries.slice';
 import userReducer from './slice/user.slice';
 
 export const store = configureStore({
   reducer: {
-    [productsApi.reducerPath]: productsApi.reducer,
     countriesReducer,
     userReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
